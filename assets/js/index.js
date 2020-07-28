10c75b4cf71610abf19801d6a0bd13761113be0c
Melánie <lezakova.melanie@gmail.com>
2020-07-28 18:43:31 +0200

    layout edits

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,3 +31,24 @@ window.addEventListener("scroll", function () {
     $("#wave-2-down").css("left", waveOffset.left - st / 10);
   }
 });*/
+
+function getHeight() {
+  var sec = document.getElementById("pool-section"),
+    content = document.getElementById("pool-content");
+
+  var bodyRect = document.body.getBoundingClientRect(),
+    secRect = sec.getBoundingClientRect(),
+    contentRect = content.getBoundingClientRect(),
+    secOffset = secRect.top - bodyRect.top,
+    conOffset = contentRect.top - bodyRect.top;
+  var conTotal = content.offsetHeight + conOffset;
+  var secTotal = sec.offsetHeight + secOffset;
+  var plusHeight = 0;
+
+  if (conTotal > secTotal) {
+    plusHeight = conTotal - secTotal;
+    sec.style.minHeight = sec.offsetHeight + plusHeight + "px";
+  }
+}
+document.addEventListener("DOMContentLoaded", getHeight);
+document.addEventListener("resize", getHeight);
