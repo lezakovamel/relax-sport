1e7585988e28dd8076a9137b217cdf7784d370a4
Melánie <lezakova.melanie@gmail.com>
2020-08-09 18:45:09 +0200

    minor responsivity changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,3 @@
-/*var window_width = $(window).width() - $(".wave-img").width();
-
-var document_height = $(document).height() - $(window).height();
-
-window.addEventListener("scroll", function () {
-  var element = document.querySelector(".wave-img");
-  var element = document.getElementById("wave-1-up");
-  var position = element.getBoundingClientRect();
-  var offset = $(".inner").offset();
-  var waveOffset = $(".wave-content").offset();
-  console.log(`wave left: ${waveOffset.left}`);
-  console.log(`offset left: ${offset.left}`);
-  console.log(`position top ${position.top}`);
-
-  // checking whether fully visible
-  if (position.top >= 0 && position.bottom <= window.innerHeight) {
-    console.log("Element is fully visible in screen");
-  }
-
-  // checking for partial visibility
-  if (position.top < window.innerHeight && position.bottom >= 0) {
-    var st = $(this).scrollTop();
-    console.log(`st: ${st}`);
-    console.log("Element is partially visible in screen");
-
-    $("#wave-1-up").css("left", waveOffset.left - st / 10);
-
-    $("#wave-2-up").css("left", waveOffset.left - st / 10);
-
-    $("#wave-1-down").css("left", waveOffset.left - st / 10);
-    $("#wave-2-down").css("left", waveOffset.left - st / 10);
-  }
-});*/
-
 function getHeight() {
   var sec = document.getElementById("pool-section"),
     content = document.getElementById("pool-content");
@@ -46,7 +12,7 @@ function getHeight() {
   var plusHeight = 0;
 
   if (conTotal > secTotal) {
-    plusHeight = conTotal - secTotal;
+    plusHeight = conTotal - secTotal - 100;
     sec.style.minHeight = sec.offsetHeight + plusHeight + "px";
   }
 }
